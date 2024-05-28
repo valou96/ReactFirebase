110021adc85ee1442a3538a076da183070f7ec44
F110-004\bachelor <esteban.piqueras@saintmichelannecy.fr>
2024-05-28 16:45:25 +0200

    dev

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDzNL28xFIdmM0gBmIaNyVTi_0hwLZeYY4",
+  authDomain: "quiz-153c5.firebaseapp.com",
+  projectId: "quiz-153c5",
+  storageBucket: "quiz-153c5.appspot.com",
+  messagingSenderId: "277624597147",
+  appId: "1:277624597147:web:909a0e0ef221fbb4f3f43e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
